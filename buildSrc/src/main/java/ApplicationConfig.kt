d9c51e3302b14f27ac477f8384145a45a49d5836
Lucas Nepomuceno Santos <lucas.nepomuceno1999@gmail.com>
2021-02-06 17:45:02 -0300

    basic config to defaultConfig main build.gradler

--- buildSrc/src/main/java/ApplicationConfig.kt
+++ buildSrc/src/main/java/ApplicationConfig.kt
@@ -1,2 +1,12 @@
 object ApplicationConfig {
+
+    const val compileSdkVersion = 30
+    const val buildToolsVersion = "30.0.3"
+
+    const val applicationId = "com.lnsantos.quadrodedesenho"
+    const val minSdkVersion = 22
+    const val targetSdkVersion = 30
+    const val versionCode = 1
+    const val versionName = "1.0"
+
 }
